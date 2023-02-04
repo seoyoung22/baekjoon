c2b893aa301aaaffb0ac656c818c8b4793ed5883
Seoyoung <102525318+seoyoung22@users.noreply.github.com>
2023-02-05 02:13:56 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n, int k) {
+    int answer = 0;
+    int service = 0;
+    service=n/10;
+    answer=n*12000+(k-service)*2000;
+    return answer;
+}
